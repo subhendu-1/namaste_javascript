03c3e93fc6a4897fe11246552938325f8d601085
Subhendu Adak <Subhenduadak54@gmail.com>
2023-09-23 20:36:47 +0530

    Global execution context and Hostin in javaScript

---
+++ fun_work.js
@@ -0,0 +1,13 @@
+let x = 20;
+a();
+b();
+console.log(x);
+
+function a(){
+    let x = 100;
+    console.log(x);
+}
+function b(){
+    let x = 200;
+    console.log(x);
+}
---
+++ index.js
@@ -0,0 +1,13 @@
+// getName();
+// console.log(x);
+let x = 2;
+function getName1(){
+    console.log("Namasta javacript1");
+}
+let getName = () =>{
+    console.log("Namasta javacript");
+}
+
+getName1();
+console.log(x);
+console.log(getName);
