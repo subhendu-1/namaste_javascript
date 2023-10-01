56cfff057532f34283b4e1fa8c28430e0274b13d
Subhendu Adak <Subhenduadak54@gmail.com>
2023-10-01 09:49:53 +0530

    var,let & const datatype in JS

--- const_var.js
+++ const_var.js
@@ -1,7 +1,18 @@
-var x = 10;
-let x = 20;
-let x;  // in let datetype not give it to value in inetializtion this is correct
+// var x = 10;
+// let x = 20;
+// let x;  // in let datetype not give it to value in inetializtion this is correct
 
-x = 30
-const x =30;
+// x = 30
+// const x =30;
 // const x; // This is give it to error in const you can't change the value after ones intialize
+console.log(y);
+// let y = 22;
+var y = 22;
+console.log(y);
+
+{
+  let  y = 33;
+    console.log(y);
+}
+
+console.log(y);
