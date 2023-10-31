a2a7266144a648f6b02a9112e53de7c16843533b
Subhendu Adak <Subhenduadak54@gmail.com>
2023-10-31 21:50:42 +0530

    block in JS

---
+++ block_scope&shadwing.js
@@ -0,0 +1,6 @@
+// block :- we can write code in block using {} here we can strore many statement together in a gorup and wehere javascpript is aspect one statement we use it
+
+{
+    let a = 20;
+    console.log(a);
+}
